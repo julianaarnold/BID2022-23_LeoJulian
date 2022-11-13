0ef13b15be1034133519c16974a02fac9451a5fb
ldreessen <leonard.dreessen@student.hpi.uni-potsdam.de>
2022-11-13 21:34:15 +0100

    assignment 3+

--- assignment01/src/assignment03/calibration.cpp
+++ assignment01/src/assignment03/calibration.cpp
@@ -44,11 +44,20 @@ bool HomograpyCalibrator::calibrationComplete() const
 
 void HomograpyCalibrator::computeHomography()
 {
-    /*~~~~~~~~~~~*
-     * YOUR CODE *
-     * GOES HERE *
-     *~~~~~~~~~~~*/
+    // scaling???
+    float scaleFactor = 1 / (m_canvas.rows / m_canvas.cols);
+    std::vector<cv::Point2f> scaledCameraCorners();
+    for (auto &corner: cameraCorners()) {
+        scaledCameraCorners.push_back(scaleFactor * corner);
+    }
 
+    std::vector<cv::Point2f> scaledProjectorCorners();
+    for (auto &corner: projectorCorners()) {
+        scaledProjectorCorners.push_back(scaleFactor * corner);
+    }
+
+    m_cameraToGame = cv::getPerspectiveTransform(scaledCameraCorners(), Game::gameCorners());
+    m_gameToProjector = cv::getPerspectiveTransform(gameCorners(), scaledProjectorCorners());
     // You should assign something to m_cameraToGame and m_gameToProjector.
     // For this, you can use cameraCorners(), projectorCorners() and Game::gameCorners().
     // Tip: The camera and projector corners are given in window resolution (probably something like 800x600, check this using m_canvas.cols/rows),
@@ -57,11 +66,14 @@ void HomograpyCalibrator::computeHomography()
 
 cv::Point2f HomograpyCalibrator::cameraToGame(const cv::Point2f& point) const
 {
-    /*~~~~~~~~~~~*
-     * YOUR CODE *
-     * GOES HERE *
-     *~~~~~~~~~~~*/
+    cv::Mat <float> pointMatrix(3,1);
+    pointMatrix(0,0) = point.x; 
+    pointMatrix(1,0) = point.y; 
+    pointMatrix(2,0) = 1.0;
+    // inverse???
+    cv::Point2f transformedPoint = m_cameraToGame * pointMatrix;
 
+    return transformedPoint;
     // Use m_cameraToGame to transform the point.
     // Tip: You can only multiply matrices with each other in OpenCV, so you'll have to build one (or build a cv::Vec3 and convert that).
     // Also, matrix multiplication isn't commutative.
@@ -69,10 +81,7 @@ cv::Point2f HomograpyCalibrator::cameraToGame(const cv::Point2f& point) const
 
 void HomograpyCalibrator::gameToProjector(const cv::Mat& input, cv::Mat& output) const
 {
-    /*~~~~~~~~~~~*
-     * YOUR CODE *
-     * GOES HERE *
-     *~~~~~~~~~~~*/
+    cv::warpPerspective (InputArray src, OutputArray dst, InputArray M, Size dsize, int flags=INTER_LINEAR, int borderMode=BORDER_CONSTANT, const Scalar &borderValue=Scalar())
 
     // Use m_gameToProjector to transform the image.
 }
