55a6fd62bc883d8695df0b0f5cb8b353d327ab8d
Julian Arnold <julian.arnold@student.hpi.de>
2022-11-14 22:24:30 +0100

    fix stuff
    Co-authored-by: Leonard Dreeßen <ldreessen@users.noreply.github.com>

--- assignment01/src/assignment03/multitouchGameControl.cpp
+++ assignment01/src/assignment03/multitouchGameControl.cpp
@@ -7,43 +7,13 @@ MultitouchGameControl::MultitouchGameControl(Game& game, HomograpyCalibrator& ca
 
 void MultitouchGameControl::handleInput(std::vector<cv::RotatedRect> positions)
 {
-    // iterate over players
-   
+    for (auto i = 0; i < positions.size(); i++) {
+        // decide which player this belongs to
+        auto position = m_calibrator.cameraToGame(positions[i].center);
+        auto playerIndex = static_cast<int>(position.x > Game::gameCanvasSize().width / 2.0);
+        auto& player = m_game.players()[playerIndex];
 
-    for (auto &player : m_game.players()) {
-        auto paddles = player.paddles();
-        for (int i = 0; i < 2; i++) {
-            auto * paddle = &paddles[i];
-
-            std::vector<cv::Point2f> input = std::vector<cv::Point2f>();
-            for (auto &position: positions) {
-                cv::Point2f center = position.center;
-                center = m_calibrator.cameraToGame(center);
-
-                float diffX = center.x - (m_game.gameCanvasSize().width / 2);
-                float diffY = center.y - (m_game.gameCanvasSize().height / 2);
-                float playerDirection = diffX * paddle->direction();
-
-                if ((playerDirection < 0) && (diffY * (2 * i - 1) < 0)) {
-                    input.push_back(center);
-                }
-            }
-
-            if (!input.empty()) {
-                printf("set paddle %d to %f \n", i, input[0].y);
-                paddle->setActive(true);
-                printf("paddle is active %d \n", paddle->active());
-                paddle->setTargetPosition(input[0].y);
-            } else {
-                //paddle->setActive(false);
-            }
-        }
+        auto paddleIndex = static_cast<int>(position.y > Game::gameCanvasSize().height / 2.0);
+        player.paddles()[paddleIndex].setTargetPosition(position.y);
     }
-    /*~~~~~~~~~~~*
-     * YOUR CODE *
-     * GOES HERE *
-     *~~~~~~~~~~~*/
-
-    // Use m_calibrator to access and modify the m_game's player's pedals.
-    // How you do it is your call, but you should probably use active(), setActive(), position() and setTargetPosition().
 }
